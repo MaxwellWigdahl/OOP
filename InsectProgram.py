935ce26d7bf8598e9a0d935f53cfdd42964a51e9
Max <max_wigdahl1@baylor.edu>
2024-02-21 15:30:28 -0600

    OOP

--- InsectProgram.py
+++ InsectProgram.py
@@ -4,6 +4,6 @@ def main():
     my_insect = ic.Insect()
     my_insect.flight()
 
-    print('This is the number of miles that the insect traveled:', my_insect.get_miles())
+    print('This is the number of miles that the insect can fly:', my_insect.get_miles())
 
 main()
